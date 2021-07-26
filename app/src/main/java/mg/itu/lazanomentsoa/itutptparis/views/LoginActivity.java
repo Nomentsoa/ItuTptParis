5314497c1712a51d726ebe511e370236c2351024
Laza Nomentsoa <starksedera@gmail.com>
2021-07-26 13:30:12 +0300

    design_login_activity

---
+++ app/src/main/java/mg/itu/lazanomentsoa/itutptparis/views/LoginActivity.java
@@ -0,0 +1,16 @@
+package mg.itu.lazanomentsoa.itutptparis.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mg.itu.lazanomentsoa.itutptparis.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
