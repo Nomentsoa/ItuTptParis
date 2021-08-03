9c2dd2ee1d7be9b85f1b42c0c787574c30b9d887
Laza Nomentsoa <starksedera@gmail.com>
2021-08-03 13:22:41 +0300

    init scanne qr code

--- app/src/main/java/mg/itu/lazanomentsoa/itutptparis/views/fragments/QrcodeFragment.java
+++ app/src/main/java/mg/itu/lazanomentsoa/itutptparis/views/fragments/QrcodeFragment.java
@@ -1,10 +1,27 @@
 package mg.itu.lazanomentsoa.itutptparis.views.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+import mg.itu.lazanomentsoa.itutptparis.R;
 import mg.itu.lazanomentsoa.itutptparis.databinding.FragmentQrcodeBinding;
 import mg.itu.lazanomentsoa.itutptparis.views.AbstractBaseFragment;
 
@@ -12,6 +29,13 @@ import mg.itu.lazanomentsoa.itutptparis.views.AbstractBaseFragment;
 public class QrcodeFragment extends AbstractBaseFragment {
     private FragmentQrcodeBinding binding;
 
+
+    SurfaceView surfaceView;
+    TextView txtBarcodeValue;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+    String intentData = "";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,7 +49,82 @@ public class QrcodeFragment extends AbstractBaseFragment {
         binding = FragmentQrcodeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        initViews(root);
+        initialiseDetectorsAndSources();
 
         return root;
     }
+
+
+    private void initViews(View view) {
+        txtBarcodeValue = view.findViewById(R.id.txtBarcodeValue);
+        surfaceView = view.findViewById(R.id.surfaceView);
+    }
+
+    private void initialiseDetectorsAndSources() {
+
+        Toast.makeText(getContext(), getResources().getText(R.string.scanne_qr_code), Toast.LENGTH_SHORT).show();
+        barcodeDetector = new BarcodeDetector.Builder(getContext())
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        cameraSource = new CameraSource.Builder(getContext(), barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceView.getHolder());
+                    } else {
+                        ActivityCompat.requestPermissions(getActivity(), new
+                                String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                Toast.makeText(getContext(), getResources().getText(R.string.scanne_qr_code_arreter), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+                    txtBarcodeValue.post(new Runnable() {
+                        @Override
+                        public void run() {
+                                intentData = barcodes.valueAt(0).displayValue;
+                                txtBarcodeValue.setText(intentData);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+
+
+
+
 }
