652dea8e3f5e953494bfb599e80df192314a6dab
Laza Nomentsoa <starksedera@gmail.com>
2021-08-09 13:51:54 +0300

    modification ecran login

---
+++ app/src/main/java/mg/itu/lazanomentsoa/itutptparis/views/InscriptionActivity.java
@@ -0,0 +1,16 @@
+package mg.itu.lazanomentsoa.itutptparis.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import mg.itu.lazanomentsoa.itutptparis.R;
+
+public class InscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+    }
+}
