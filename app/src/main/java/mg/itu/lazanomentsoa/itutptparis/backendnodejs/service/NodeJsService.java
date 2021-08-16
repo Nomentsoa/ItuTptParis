9f3d724d6b01cf5813f7229a9683d83383a4f1cd
Laza Nomentsoa <starksedera@gmail.com>
2021-08-17 01:10:40 +0300

    url prod

--- app/src/main/java/mg/itu/lazanomentsoa/itutptparis/backendnodejs/service/NodeJsService.java
+++ app/src/main/java/mg/itu/lazanomentsoa/itutptparis/backendnodejs/service/NodeJsService.java
@@ -7,8 +7,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NodeJsService {
    // private static final  String BASE_URL_NODEJS = "http://10.163.1.166:3000/api/";
-    private static final  String BASE_URL_NODEJS = "http://192.168.1.135:3000/api/";
+    //private static final  String BASE_URL_NODEJS = "http://192.168.1.135:3000/api/";
     //private static final  String BASE_URL_NODEJS = "http://192.168.43.165:3000/api/";
+    private static final  String BASE_URL_NODEJS = "https://mbdsp7-node-tpt-pari.herokuapp.com/api/";
 
     private static Retrofit retrofitNodeJs = new Retrofit.Builder()
             .baseUrl(BASE_URL_NODEJS)
